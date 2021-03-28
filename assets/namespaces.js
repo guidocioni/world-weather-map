31c7d600837d4cfd5dcba32a91d1232810d218f5
Guido Cioni <guidocioni@gmail.com>
2021-03-28 17:33:54 +0200

    Change visualisation to scatter with text inside Optimized style Added new assets folder with needed functions

---
+++ assets/namespaces.js
@@ -0,0 +1,93 @@
+// require('./style.css')
+
+// region Leaflet extensions
+
+L.DivIcon.Scatter = L.DivIcon.extend({
+    createIcon: function(oldIcon) {
+           let icon = L.DivIcon.prototype.createIcon.call(this, oldIcon);
+           icon.style.backgroundColor = this.options.color;
+           return icon;
+    }
+})
+
+L.divIcon.scatter = function(opts) {
+    return new L.DivIcon.Scatter(opts);
+}
+
+// endregion
+
+function resolve(primary, fallback, keys){
+    const obj = {}
+    for(let i = 0; i < keys.length; i++){
+        obj[keys[i]] = (primary && primary[keys[i]])? primary[keys[i]] : fallback[keys[i]]
+    }
+    return obj
+}
+
+// region Color
+
+const colorDefaults = {min:0, max:1, colorscale:['yellow', 'red', 'black']}
+
+function getColorContinuous(options, value){
+    const {min, max, colorscale} = resolve(options, colorDefaults, ["min", "max", "colorscale"])
+    const csc = chroma.scale(colorscale).domain([min, max])
+    return csc(value)
+}
+
+function getColorDiscrete(options, value) {
+    const {classes, colorscale} = resolve(options, colorDefaults, ["classes", "colorscale"])
+    let color = null;
+    for (let i = 0; i < classes.length; ++i) {
+        if (value > classes[i]) {
+            color = colorscale[i]
+        }
+    }
+    return color
+}
+
+function getColor(options, value){
+    const {classes} = resolve(options, colorDefaults, ["classes"])
+    if(classes){
+        return getColorDiscrete(options, value)
+    }
+    return getColorContinuous(options, value)
+}
+
+// endregion
+
+const scatterDefaults = {colorProp: "value", circleOptions: {fillOpacity:1, stroke:false, radius:8}};
+
+window.myNamespace = Object.assign({}, window.myNamespace, {
+    mySubNamespace: {
+        pointToLayer: function(feature, latlng, context){
+            const {circleOptions, colorProp} = resolve(context.props.hideout, scatterDefaults,
+                ["circleOptions", "colorProp"])
+            label = String(feature.properties[colorProp].toFixed(0))
+            const icon = L.divIcon.scatter({
+                html: '<div><span>' + label + '</span></div>',
+                iconSize: L.point(20, 20),
+                className: "marker-modified",
+                color: getColor(context.props.hideout, feature.properties[colorProp])
+            });
+            return L.marker(latlng, {icon : icon})
+        },
+
+        clusterToLayer: function (feature, latlng, index, context) {
+            const {circleOptions, colorProp} = resolve(context.props.hideout, scatterDefaults,
+                ["circleOptions", "colorProp"])
+            const leaves = index.getLeaves(feature.properties.cluster_id);
+            // Choose just one value to be representative
+            leave = leaves[0]
+            console.log(leave)
+            label = String(leave.properties[colorProp].toFixed(0))
+            const icon = L.divIcon.scatter({
+                html: '<div><span>' + label + '</span></div>',
+                iconSize: L.point(20, 20),
+                className: "marker-modified",
+                color: getColor(context.props.hideout, leave.properties[colorProp])
+            });
+            return L.marker(latlng, {icon : icon})
+        }
+
+    },
+});
--- webapp_it.py
+++ webapp_it.py
@@ -20,13 +20,12 @@ mapURL = 'https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}{r}?
 attribution = '© <a href="https://www.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 
 
-
 with open("provinces_it.geojson", 'r') as f:
     statesData = json.load(f)
 
 
 # Setup a few color scales.
-csc_map = {"Rainbow": ['purple', 'blue', 'green', 'yellow', 'red'], 
+csc_map = {"Rainbow": ['purple', 'blue', 'green', 'yellow', 'red'],
            "Hot": ['yellow', 'red', 'black'],
            "Viridis": "Viridis"}
 csc_options = [dict(label=key, value=json.dumps(csc_map[key]))
@@ -37,34 +36,37 @@ dd_csc = dcc.Dropdown(options=csc_options, value=json.dumps(
 
 default_variable = "temperature"
 variable_options = [
-{'label': 'temperature', 'value': 'temperature'},
-{'label': 'pressure', 'value': 'smlp'},
-{'label': 'humidity', 'value': 'rh'},
-{'label': 'wind_speed', 'value': 'wind_speed'},
-{'label': 'wind_gust', 'value': 'wind_gust'},
-{'label': 'rain_rate', 'value': 'rain_rate'},
-{'label': 'daily_rain', 'value': 'daily_rain'},
-{'label': 'dew_point', 'value': 'dew_point'},
-{'label': 'rad', 'value': 'rad'},
+    {'label': 'Temp.', 'value': 'temperature'},
+    {'label': 'MSLP', 'value': 'smlp'},
+    {'label': 'Hum.', 'value': 'rh'},
+    {'label': 'Wind Sp.', 'value': 'wind_speed'},
+    {'label': 'Wind G.', 'value': 'wind_gust'},
+    {'label': 'Rain', 'value': 'rain_rate'},
+    {'label': 'Daily Rain', 'value': 'daily_rain'},
+    {'label': 'Dewp', 'value': 'dew_point'},
+    {'label': 'Rad', 'value': 'rad'},
 ]
 
 
-ns = Namespace("dlx", "scatter")
+ns = Namespace("myNamespace", "mySubNamespace")
 geojson_countries = dl.GeoJSON(data=statesData, id="geojson_countries",
-     options=dict(style=dict(weight=1, opacity=0.7, color='white', fillOpacity=0)))
+                               options=dict(style=dict(weight=1, opacity=0.7, color='white', fillOpacity=0)))
 
 # Create the app.
 chroma = "https://cdnjs.cloudflare.com/ajax/libs/chroma-js/2.1.0/chroma.min.js"
-app = Dash(external_scripts=[chroma], prevent_initial_callbacks=True, url_base_pathname='/weathermap-it/')
+app = Dash(__name__,
+           external_scripts=[chroma],
+           prevent_initial_callbacks=True,
+           url_base_pathname='/weathermap-it/')
 server = app.server
 
-cache = Cache(server, config={'CACHE_TYPE': 'filesystem', 
+cache = Cache(server, config={'CACHE_TYPE': 'filesystem',
                               'CACHE_DIR': '/tmp'})
 
 
 @cache.memoize(900)
 def get_df():
-    df = mnw.get_realtime_stations(country='IT')
+    df = mnw.get_realtime_stations(country="IT")
 
     return df
 
@@ -73,11 +75,14 @@ def get_df():
 def get_data(variable):
     df = get_df().dropna(subset=[variable])
     # drop irrelevant columns
-    df = df[['latitude', 'longitude', 'altitude', 'name', 'observation_time_local', variable]]
+    df = df[['latitude', 'longitude', 'altitude',
+             'name', 'observation_time_local', variable]]
     dicts = df.to_dict('rows')
     for item in dicts:
-        item["tooltip"] = "Measured at %s : %2.1f" % (item["observation_time_local"], item[variable])
-        item["popup"] = "%s, altitude: %4.0f m" %  (item["name"], item["altitude"])
+        item["tooltip"] = "Measured at %s : %2.1f" % (
+            item["observation_time_local"], item[variable])
+        item["popup"] = "%s, altitude: %4.0f m" % (
+            item["name"], item["altitude"])
     geojson = dlx.dicts_to_geojson(dicts, lon="longitude", lat="latitude")
     geobuf = dlx.geojson_to_geobuf(geojson)
 
@@ -87,79 +92,80 @@ def get_data(variable):
 @cache.memoize(900)
 def get_minmax(variable):
     df = get_df().dropna(subset=[variable])
-    df_variable = df[variable] 
+    df_variable = df[variable]
 
     return dict(min=df_variable.values.min(), max=df_variable.values.max())
 
 
 def serve_layout():
     dd_variable = dcc.Dropdown(options=variable_options,
-                           value=default_variable, id="dd_variable", clearable=False)
+                               value=default_variable, id="dd_variable", clearable=False)
     minmax = get_minmax(default_variable)
     # # Create a colorbar.
     colorbar = dl.Colorbar(
-    colorscale=csc_map[default_csc], id="colorbar", width=20, height=150, **minmax)
+        colorscale=csc_map[default_csc], id="colorbar", width=20, height=150, **minmax)
 
-    geojson = dl.GeoJSON(data=get_data(default_variable), id="geojson", format="geobuf",
-                     #zoomToBounds=True,  # when true, zooms to bounds when data changes
-                     cluster=True,  # when true, data are clustered
-                     # how to draw clusters
-                     clusterToLayer=ns("clusterToLayer"),
-                     # when true, zooms to bounds of feature (e.g. cluster) on click
-                     zoomToBoundsOnClick=True,
-                     # how to draw points
-                     options=dict(pointToLayer=ns("pointToLayer")),
-                     superClusterOptions=dict(radius=0.1),  # adjust cluster size
-                     hideout=dict(colorscale=csc_map[default_csc], colorProp=default_variable, **minmax))
+    geojson = dl.GeoJSON(data=get_data(default_variable), id="geojson",
+                         format='geobuf',
+                         cluster=True,  # when true, data are clustered
+                         # how to draw clusters
+                         clusterToLayer=ns("clusterToLayer"),
+                         # how to draw points
+                         options=dict(pointToLayer=ns("pointToLayer")),
+                         superClusterOptions=dict(radius=50),  # adjust cluster size
+                         hideout=dict(colorscale=csc_map[default_csc], colorProp=default_variable, **minmax))
 
     times = pd.date_range(start=pd.to_datetime('now', utc=True).round('15min') - pd.Timedelta('2hours'),
-              end=pd.to_datetime('now', utc=True).round('15min') - pd.Timedelta('15min'),
-              freq='5min')
+                          end=pd.to_datetime('now', utc=True).round(
+                              '15min') - pd.Timedelta('15min'),
+                          freq='5min')
     latest = times[-1].strftime('%Y-%m-%dT%H:%M:00.000Z')
 
-    numdate= [x for x in range(len(times))]
-    marks = {numd:date.strftime('%H:%M') for numd, date in zip(numdate, times)}
+    numdate = [x for x in range(len(times))]
+    marks = {numd: date.strftime('%H:%M')
+             for numd, date in zip(numdate, times)}
 
     slider = dcc.Slider(id='time_slider', min=numdate[0],
-                                          max=numdate[-1],
-                                          value=numdate[-1],
-                                          marks=marks)
-
+                        max=numdate[-1],
+                        value=numdate[-1],
+                        marks=marks)
 
     layout = html.Div([
         dl.Map([
             dl.LayersControl(
-            [
-            dl.BaseLayer(dl.TileLayer(url=mapURL, attribution=attribution,
-                                      tileSize=512, zoomOffset=-1), name='map', checked=True),
-            dl.Overlay(dl.WMSTileLayer(url="https://maps.dwd.de/geoserver/ows?",
-                                            layers="dwd:SAT_EU_RGB", 
-                                            format="image/png", 
-                                            transparent=True, opacity=0.6,
-                                            version='1.3.0',
-                                            detectRetina=True), name='sat eu', checked=True),
-            dl.Overlay(dl.WMSTileLayer(id='radar_it',
-                                       url="http://www.protezionecivile.gov.it/geowebcache/service/wms?tiled=True&time=%s" % latest,
-                                       layers="radar:vmi",
-                                       transparent=True,
-                                       format="image/png",
-                                       opacity=1.0,
-                                       version='1.1.1'),
+                [
+                    dl.BaseLayer(dl.TileLayer(url=mapURL, attribution=attribution,
+                                              tileSize=512, zoomOffset=-1), name='map', checked=True),
+                    dl.Overlay(dl.WMSTileLayer(url="https://maps.dwd.de/geoserver/ows?",
+                                               layers="dwd:SAT_EU_RGB",
+                                               format="image/png",
+                                               transparent=True, opacity=0.6,
+                                               version='1.3.0',
+                                               detectRetina=True), name='sat eu', checked=True),
+                    dl.Overlay(dl.WMSTileLayer(id='radar_it',
+                                               url="http://www.protezionecivile.gov.it/geowebcache/service/wms?tiled=True&time=%s" % latest,
+                                               layers="radar:vmi",
+                                               transparent=True,
+                                               format="image/png",
+                                               opacity=1.0,
+                                               version='1.1.1'),
                       name='radar IT', checked=True),
-            dl.Overlay([geojson, colorbar], name='obs', checked=True)
-            ]),
+                    dl.Overlay([geojson, colorbar], name='obs', checked=True)
+                ]),
             geojson_countries,
-            ], center=[41, 12], zoom=6),
-        html.Div(id='date-div', style={'display': 'none'}, children=[times.strftime('%Y-%m-%dT%H:%M:00.000Z')]),
+        ], center=[41, 12], zoom=6),
+        html.Div(id='date-div', style={'display': 'none'},
+                 children=times.strftime('%Y-%m-%dT%H:%M:00.000Z')),
         html.Div([slider],
-            style={"position": "absolute", "bottom": "20px",
-            "left": "10px", "z-index": "1000", "width": "800px",
-            "background-color":'rgba(1, 1, 1, 0.3)'}),
+                 style={"position": "absolute", "bottom": "20px",
+                        "left": "10px", "z-index": "1000", "width": "800px",
+                        "background-color": 'rgba(1, 1, 1, 0.3)'}),
         html.Div([dd_variable],
-            style={"position": "absolute", "top": "300px", "right": "16px", "z-index": "1000", "width": "100px"})
+                 style={"position": "absolute", "top": "300px", "right": "16px", "z-index": "1000", "width": "100px"})
     ], style={'width': '100%', 'height': '90vh', 'margin': "auto", "display": "block", "position": "relative"})
 
     return layout
+
 
 app.layout = serve_layout
 
@@ -178,10 +184,9 @@ def update(variable):
               [Input("time_slider", "value")],
               [State("date-div", "children")])
 def update_time(time, dates):
-    url = "http://www.protezionecivile.gov.it/geowebcache/service/wms?tiled=True&time=%s" % dates[0][time]
+    url = "http://www.protezionecivile.gov.it/geowebcache/service/wms?tiled=True&time=%s" % dates[time]
 
     return [url]
-
 
 
 if __name__ == '__main__':
